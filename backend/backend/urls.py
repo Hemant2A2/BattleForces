1d398edb7687c308165fed6d9a5b05c13fe12362
Hemant Ayuj <hemant.ayuj@gmail.com>
2024-10-16 00:02:37 +0530

    updated urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
-    path('api/auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
     path('', home_view, name='home'),
 ]
